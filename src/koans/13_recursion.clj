7a567bbe3889fba0ef528a4609c8c8274a20b50b
atscott <atscott01@gmail.com>
2015-03-14 09:29:50 -0500

    13-14

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -3,22 +3,32 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
-  (loop [n   n
+  (loop [n n
          acc true]
     (if (= n 0)
-      __
+      false
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+         acc '()]
+    (if (empty? coll)
+      acc
+      (recur (rest coll)
+             (conj acc (first coll)))))
+  )
 
 (defn factorial [n]
-  __)
-
+  (loop [n n
+         acc 1]
+    (if (= n 0)
+      acc
+      (recur (dec n)
+             (* acc n)))))
 (meditations
   "Recursion ends with a base case"
   (= true (is-even? 0))
@@ -52,3 +62,4 @@
 
   "But what happens when the machine limits you?"
   (< 1000000000000000000000000N (factorial 100003N)))
+
