15bfdfca361d5e57f79261c1195f83a3faf84cce
atscott <atscott01@gmail.com>
2015-03-17 15:08:04 -0500

    using thread macro for beautiful Clojure

--- src/koans/14_destructuring.clj
+++ src/koans/14_destructuring.clj
@@ -3,28 +3,30 @@
 
 (def test-address
   {:street-address "123 Test Lane"
-   :city "Testerville"
-   :state "TX"})
+   :city           "Testerville"
+   :state          "TX"})
 
 (meditations
   "Destructuring is an arbiter: it breaks up arguments"
-  (=  ((fn [[a b]] (str b a))
-         [:foo :bar]))
+  (= ((fn [[a b]] (str b a))
+       [:foo :bar]))
 
   "Whether in function definitions"
   (= (str "First comes love, "
           "then comes marriage, "
           "then comes Clojure with the baby carriage")
      ((fn [[a b c]] (str "First comes " a ", "
-                     "then comes " b ", "
-                      "then comes " c " with the baby carriage"))
-      ["love" "marriage" "Clojure"]))
+                         "then comes " b ", "
+                         "then comes " c " with the baby carriage"))
+       ["love" "marriage" "Clojure"]))
 
   "Or in let expressions"
   (= "Rich Hickey aka The Clojurer aka Go Time aka Macro Killah"
      (let [[first-name last-name & aliases]
            (list "Rich" "Hickey" "The Clojurer" "Go Time" "Macro Killah")]
-       (apply str (interpose " aka " (cons (str first-name " " last-name) aliases)))))
+       (->> (cons (str first-name " " last-name) aliases)
+            (interpose " aka ")
+            (apply str))))
 
   "You can regain the full argument if you like arguing"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
@@ -39,12 +41,15 @@
   "Or more succinctly"
   (= "123 Test Lane, Testerville, TX"
      (let [{:keys [street-address city state]} test-address]
-       (apply str (interpose ", " (list street-address city state)))))
+       (->> (list street-address city state)
+            (interpose ", ")
+            (apply str))))
 
   "All together now!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX"
      ((fn [[a b] {:keys [street-address city state]}]
-        (apply str (interpose ", " (list (str a " " b) street-address city state))))
-       ["Test" "Testerson"] test-address))
-  )
-
+        (->> (list (str a " " b) street-address city state)
+             (interpose ", ")
+             (apply str)))
+       ["Test" "Testerson"] test-address)
+  ))
